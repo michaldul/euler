c390e1e1f3cfc1c4093207b878bb97ac7cfb8a8e
Michał <michaldul@outlook.com>
2014-09-02 22:48:39 +0200

    projecteuler.net/problem=44

---
+++ 44.fs
@@ -0,0 +1,11 @@
+let isInteger x = x = (float (int32 x))
+let isPentagonal x = isInteger ((sqrt (1. + 24. * float x) + 1.) / 6.)
+let test (a, b) = isPentagonal (a - b) && isPentagonal (a + b)
+
+let gen n = n * (3 * n - 1)/2
+let pentagonalPairs = 
+    seq { for n in 1 .. 1000000 do 
+            for m in 1 .. n do 
+                yield (gen n, gen m) }
+
+match (Seq.find test pentagonalPairs) with | (a, b) -> printfn "%d" (a - b)
