86c4457098808f1c72a469cb099740970086db44
Michał <michaldul@outlook.com>
2016-04-10 22:08:55 +0200

    p67, bottom-up approach

--- problem67.py
+++ problem67.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 
 
-def find_max_path(triangle):
-    row_best = [triangle[0][0]]  # stores max scores for each position in processed row
-    for row in triangle[1:]:
-        zero_bounded = [0] + row_best + [0]
+def find_max_path(reversed_triangle):
+    row_best = reversed_triangle[0]
+    for row in reversed_triangle[1:]:
         new_best = []
         for i in range(len(row)):
-            new_best.append(max(row[i] + zero_bounded[i], row[i] + zero_bounded[i + 1]))
-
+            new_best.append(row[i] + max(row_best[i], row_best[i+1]))
         row_best = new_best
 
-    return max(row_best)
+    return row_best
 
 
 with open('p067_triangle.txt', 'r') as file:
     lines = file.readlines()
     triangle = [[int(s_num) for s_num in line.split()] for line in lines]
-    print(find_max_path(triangle))
+    reversed_triangle = list(reversed(triangle))
+    print(find_max_path(reversed_triangle))
